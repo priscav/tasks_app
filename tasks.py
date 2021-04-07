bdc87c5c1286e85b695637e52621b37a8c9537db
Pris Avila <priscavila71@gmail.com>
2021-04-07 16:40:41 +0200

    fix#2

--- tasks.py
+++ tasks.py
@@ -45,39 +45,41 @@ def create_task(user_login):
         print("\nThis task has already been  created\n")
 
 def update_state(user_login):
-    all_tasks_user(user_login)
+    tasks = all_tasks_user(user_login)
     UPDATE_TASK = """\n
     Task Pending: select 1
     Task On-going: select 2
     Task Finished: select 3
     \n"""
-    id_task = input("Task id: ")
-    task = db.session.query(Task).filter_by(id=id_task, user_id=user_login).first()
-    if task == None:
-        print("\nThis task does not exists\n")
-    else:
-        selection_update = input(UPDATE_TASK)
-        db.session.query(Task).filter_by(id=id_task).update({Task.state_id: int(selection_update)})
-        db.session.commit()
-        print("\nTitle task has been updated\n")
+    if len(tasks)> 0:
+        id_task = input("Task id: ")
+        task = db.session.query(Task).filter_by(id=id_task, user_id=user_login).first()
+        if task == None:
+            print("\nThis task does not exists\n")
+        else:
+            selection_update = input(UPDATE_TASK)
+            db.session.query(Task).filter_by(id=id_task).update({Task.state_id: int(selection_update)})
+            db.session.commit()
+            print("\nTitle task has been updated\n")
 
 def edit_task(user_login):
-    all_tasks_user(user_login)
-    title_task_id = int(input("Task id: "))
-    task = db.session.query(Task).filter_by(id=title_task_id, user_id=user_login).first()
-    if task != None:
-        title_or_description = input("Edit title or description (T/D)?: ")
-        if title_or_description.lower() == "t":
-            new_title = input("New title: ")
-            db.session.query(Task).filter_by(title=task.title, user_id=user_login).update({Task.title: new_title})
-            db.session.commit()
-            print(f"\nThe title has been changed to: {new_title}\n")
+    tasks = all_tasks_user(user_login)
+    if len(tasks)>0:
+        title_task_id = int(input("Task id: "))
+        task = db.session.query(Task).filter_by(id=title_task_id, user_id=user_login).first()
+        if task != None:
+            title_or_description = input("Edit title or description (T/D)?: ")
+            if title_or_description.lower() == "t":
+                new_title = input("New title: ")
+                db.session.query(Task).filter_by(title=task.title, user_id=user_login).update({Task.title: new_title})
+                db.session.commit()
+                print(f"\nThe title has been changed to: {new_title}\n")
 
-        if title_or_description.lower() == "d":
-            new_description = input("New description: ")
-            db.session.query(Task).filter_by(description=task.description,user_id=user_login).update({Task.description: new_description})
-            db.session.commit()
-            print(f"\nThe description has been changed  to:  {new_description}\n")
+            if title_or_description.lower() == "d":
+                new_description = input("New description: ")
+                db.session.query(Task).filter_by(description=task.description,user_id=user_login).update({Task.description: new_description})
+                db.session.commit()
+                print(f"\nThe description has been changed  to:  {new_description}\n")
 
 def delete_task(user_login):
     tasks = all_tasks_user(user_login)
